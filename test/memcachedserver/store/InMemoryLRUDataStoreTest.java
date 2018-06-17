e841ceb7cf883281380baade442aaf8d46516e42
Tianyi Wang <tianyi.wang@airbnb.com>
2018-06-16 19:04:57 -0700

    add a test case

--- test/memcachedserver/store/InMemoryLRUDataStoreTest.java
+++ test/memcachedserver/store/InMemoryLRUDataStoreTest.java
@@ -1,12 +1,29 @@
 package memcachedserver.store;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
+
+import com.google.common.collect.ImmutableList;
 
 public class InMemoryLRUDataStoreTest {
 
+  private static final int CAPACITY = 3;
+
+  private static final String KEY_1 = "key 1";
+
+  private static final Data DATA_1 = Data.of(0, 0, new Byte[] {1});
+  private static final Data DATA_2 = Data.of(0, 0, new Byte[] {2, 2});
+
+  private final InMemoryLRUDataStore store = new InMemoryLRUDataStore(CAPACITY);
+
   @Test
   public void testSet() {
+    store.set(KEY_1, DATA_1);
+    assertEquals(DATA_1, store.get(ImmutableList.of(KEY_1)).get(KEY_1));
 
+    store.set(KEY_1, DATA_2);
+    assertEquals(DATA_2, store.get(ImmutableList.of(KEY_1)).get(KEY_1));
   }
 
   @Test
