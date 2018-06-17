b84c1e3700c7fbca0dc2c16ef6153affba779405
Tianyi Wang <tianyi.wang@airbnb.com>
2018-06-16 18:45:45 -0700

    add junit jars

---
+++ test/memcachedserver/store/InMemoryLRUDataStoreTest.java
@@ -0,0 +1,41 @@
+package memcachedserver.store;
+
+import org.junit.Test;
+
+public class InMemoryLRUDataStoreTest {
+
+  @Test
+  public void testSet() {
+
+  }
+
+  @Test
+  public void testAdd() {
+
+  }
+
+  @Test
+  public void testReplace() {
+
+  }
+
+  @Test
+  public void testAppend() {
+
+  }
+
+  @Test
+  public void testPrepend() {
+
+  }
+
+  @Test
+  public void testGet() {
+
+  }
+
+  @Test
+  public void testDelete() {
+
+  }
+}
