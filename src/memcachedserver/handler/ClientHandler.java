aee4a3d89d442735d02e01338e60178c4dbce896
Tianyi Wang <tianyi.wang@airbnb.com>
2018-06-17 23:16:43 -0700

    use 2 readers

--- src/memcachedserver/handler/ClientHandler.java
+++ src/memcachedserver/handler/ClientHandler.java
@@ -1,8 +1,10 @@
 package memcachedserver.handler;
 
+import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
 
 import lombok.NonNull;
@@ -11,17 +13,26 @@ import memcachedserver.store.DataStore;
 /**
  * For handling communication with one client via {@link Socket}.
  */
-public class ClientHandler {
+public class ClientHandler implements Runnable {
   @NonNull private final Socket socket;
-  @NonNull private final DataInputStream inputStream;
+  @NonNull private final BufferedReader commandReader;
+  @NonNull private final DataInputStream dataReader;
   @NonNull private final DataOutputStream outputStream;
 
   @NonNull private final DataStore dataStore;
 
   public ClientHandler(@NonNull final Socket socket, @NonNull final DataStore dataStore) throws IOException {
     this.socket = socket;
-    this.inputStream = new DataInputStream(socket.getInputStream());
+
+    this.commandReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    this.dataReader = new DataInputStream(socket.getInputStream());
     this.outputStream = new DataOutputStream(socket.getOutputStream());
+
     this.dataStore = dataStore;
+  }
+
+  @Override
+  public void run() {
+
   }
 }
